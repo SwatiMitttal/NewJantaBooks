152a15e59ae711b8dbbd18763ee344557ef403f1
SwatiMitttal <swatimittal1981@gmail.com>
2025-03-29 12:17:36 +0530

    v1commit

--- front/src/components/Cartab.jsx
+++ front/src/components/Cartab.jsx
@@ -9,7 +9,7 @@ function Cartab(){
   const cuser=useSelector(store=>store.users.user.email)
 return (
         <>
-        <div  className= 'fixed right-0 top-0 bg-slate-900 text-white pt-6 font-semibold grid grid-rows-[60px_1fr_60px]  w-80 h-full gap-3 tranform  translate-x-0 transition-transform duration-300 '>
+        <div  className= 'fixed right-0 top-0 bg-slate-900 text-white pt-6 font-semibold grid grid-rows-[60px_1fr_60px]  lg:w-60 md:w-30 sm:w-30 h-full gap-3 tranform  translate-x-10 transition-transform duration-300 '>
             <h2   className="text-white  font-semibold p-4 text-2xl">SHOPPING CART</h2>
             <div>
             {cart.map((item,i)=>(
