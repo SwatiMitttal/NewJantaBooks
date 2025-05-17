74e6ddded355188ccc6a9c2d02d1137a67ae825b
SwatiMitttal <swatimittal1981@gmail.com>
2025-05-17 11:08:48 +0530

    'fcommit'

--- fstore1/src/components/Ruser.jsx
+++ fstore1/src/components/Ruser.jsx
@@ -7,14 +7,13 @@ import { FontAwesome5 } from "@expo/vector-icons";
 function Ruser(){
    const [user,setUser]=useState(useSelector(store=>store.users.user))
     const handleP=async()=>{
-
-        const res=await axios.post('https://newjvite3.onrender.com/ruser',user.email)
+         alert(user.email)
+        const res=await axios.post('https://newjvite3.onrender.com/ruser',{email:user.email})
         if (res.status===200){
             alert('account deleted')
         }
     }
  return(<>
-    
     <View>
         <Text>{user.email}</Text>
   <Text><FontAwesome5 name={"trash"} size={15} color={"darkgoldenrod"}     />Account</Text>
